4c6a1078be4eb30e4455160f8a13a4f45a1b0613
Jasmine Yip <yippie.web@gmail.com>
2023-06-20 18:35:34 +1000

    Uncomment code

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ document.addEventListener('DOMContentLoaded', function() {
     const zhLangSpan = document.querySelector('.lang.zh');
   
     enLangSpan.addEventListener('click', function() {
-      //   window.location.href = 'http://127.0.0.1:5500/en/';
+    //   window.location.href = 'http://127.0.0.1:5500/en/';
       window.location.href = 'https://yippieweb.github.io/WahFuCoC/en/';
     });
   
@@ -19,14 +19,6 @@ document.addEventListener('DOMContentLoaded', function() {
 var currentUrl = window.location.href;
 var cleanUrl = currentUrl.replace('index.html', '').replace('.html', '');
 window.history.replaceState({}, document.title, cleanUrl);
-
-  // usage
-  $(document).ready(function() {
-    $('.lang').on('click', function() {
-      var selectedLanguage = $(this).hasClass('en') ? 'en' : 'zh';
-      switchLanguage(selectedLanguage);
-    });
-  });
 
 // menu toggle
 
@@ -126,3 +118,30 @@ function switchImage(currentImage){
     }
     indicators();
 }
+
+// accordion headers
+var headers = ["H1","H2","H3","H4","H5","H6"];
+
+$(".accordion").click(function(e) {
+  var target = e.target,
+      name = target.nodeName.toUpperCase();
+  
+  if($.inArray(name,headers) > -1) {
+    var subItem = $(target).next();
+    
+    //slideUp all elements (except target) at current depth or greater
+    var depth = $(subItem).parents().length;
+    var allAtDepth = $(".accordion p, .accordion div").filter(function() {
+      if($(this).parents().length >= depth && this !== subItem.get(0)) {
+        return true; 
+      }
+    });
+    $(allAtDepth).slideUp("fast");
+    
+    //slideToggle target content and adjust bottom border if necessary
+    subItem.slideToggle("fast",function() {
+        $(".accordion :visible:last").css("border-radius","0 0 10px 10px");
+    });
+    $(target).css({"border-bottom-right-radius":"0", "border-bottom-left-radius":"0"});
+  }
+});
